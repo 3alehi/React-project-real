595b37ce4e4f7bedc52885930ef8668cd17e9a03
slavik <116015277+masihSlavik@users.noreply.github.com>
2023-05-21 18:44:57 +0330

    Add files via upload

---
+++ SPA with bootstrap/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Menu from './Rout/Menu'
+import About from './Rout/About'
+import Social from './Rout/social'
+import Project from './Rout/Project'
+export default function App() {
+  return (
+    <Routes>
+      <Route path='/' element={
+        <>
+              <Menu></Menu>
+        <About></About>
+        <Social></Social>
+        </>
+
+      }></Route>
+      <Route path='projects' element={
+        <>
+            <Menu></Menu>
+        <Project></Project>
+        </>
+    
+      }></Route>
+      <Route path='learning' element={
+        <Menu></Menu>
+      }></Route>
+      <Route path='*' element={<h1 style={{textAlign:"center"}}>Error page not fonud</h1>}></Route>
+    </Routes>
+
+  )
+}
